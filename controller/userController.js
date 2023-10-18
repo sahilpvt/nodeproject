d5b97488b10bf0e05401c6c295334aff0872168b
sahilpvt <sahilpvt111@gmail>
2023-10-18 19:32:13 +0530

    remove create data

--- controller/userController.js
+++ controller/userController.js
@@ -2,7 +2,6 @@ const userModel = require('../model/userModel')
 var nodemailer = require('nodemailer');
 exports.insert = async (req, res) => {
     var otp = Math.floor(1000 + Math.random() * 9000)
-
     var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -27,16 +26,16 @@ exports.insert = async (req, res) => {
     });
 
     // const data = await userModel.create(req.body)
-    // res.status(200).json({
-    //     status: "success",
-    //     data
-    // })
+    res.status(200).json({
+        status: "Send succesfully",
+        otp
+    })
 
 }
 exports.getdata = async (req, res) => {
     const data = await userModel.find()
     res.status(200).json({
-        status: "Success",
-        data,
+        status: "get OTP successfully",
+        data
     })
 }
