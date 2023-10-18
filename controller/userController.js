e4fcedae93c1d7b9f67fd012ebe838c3f6f573fb
sahilpvt <sahilpvt111@gmail>
2023-10-18 19:10:27 +0530

    adding sendmail functionality

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,37 @@
 const userModel = require('../model/userModel')
+var nodemailer = require('nodemailer');
 exports.insert = async (req, res) => {
+    otp = Math.floor(1000 + Math.random() * 9000)
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'sahilpvt111@gmail.com',
+            pass: 'jujboblbigsdbvtb'
+        }
+    });
+
+    var mailOptions = {
+        from: 'sahilpvt111@gmail.com',
+        to: req.body.email,
+        subject: 'Hey' + req.body.name,
+        text: otp.toString()
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+
     const data = await userModel.create(req.body)
     res.status(200).json({
         status: "success",
         data
     })
+
 }
 exports.getdata = async (req, res) => {
     const data = await userModel.find()
